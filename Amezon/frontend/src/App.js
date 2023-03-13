b1776631db5a28338a9a0ef85f120a1ef9c302f8
Bhanuka <dilshandissanayaka089@gmail.com>
2023-03-13 18:41:18 +0530

    Create-React-App

---
+++ Amezon/frontend/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amezona</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
